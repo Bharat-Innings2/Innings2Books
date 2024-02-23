9d48514cd1eec5868133a925dda0db4c0083af04
Bharat <bharatg@instaread.com>
2024-02-23 16:32:12 +0530

    Initial commit, webview loaded with innings2 website and expo updte check

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Platform, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import * as Updates from 'expo-updates';
 
 export default function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // Handle error
+      console.error(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
+  const urlToLoad = 'https://books.innings2.com/';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ width: '100%', height: '100%', padding: 8 }}>
+        <WebView source={{ uri: urlToLoad }} onLoad={() => console.log('url loaded!')} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Platform.OS === 'ios' ? 50 : 28,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
